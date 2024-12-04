537573381cd24ea5d0e830958c4a44eb36ba30c3
techguy200 <owenfriedman07@gmail.com>
2024-12-04 14:28:12 -0500

    flask and starting to parse the json

---
+++ flaskr/Home.py
@@ -0,0 +1,14 @@
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, url_for
+)
+from werkzeug.exceptions import abort
+
+from flaskr.auth import login_required
+from flaskr.db import get_db
+
+bp = Blueprint('home', __name__)
+
+@bp.route('/')
+def index():
+    db = get_db()
+    return render_template('home/Home.html')
---
+++ testParser.py
@@ -0,0 +1,38 @@
+import json
+import re
+class logHandler:
+
+    def extract_card_data(file_path):
+        with open(file_path, 'r') as file:
+          log_data = file.read()
+
+        
+        match = re.search(r'Event_SetDeckV2.*?({.*})', log_data)
+        if not match:
+            print("No deck data found.")
+            return
+    
+        
+        json_data = match.group(1)
+    
+        
+        try:
+            deck_data = json.loads(json.loads(json_data)["request"])
+            main_deck = deck_data["Deck"].get("MainDeck", [])
+            sideboard = deck_data["Deck"].get("Sideboard", [])
+        
+            
+            all_cards = main_deck + sideboard
+
+            arr=[]
+            for card in all_cards:
+                arr.append([card['cardId'],card['quantity']])
+            return(arr)
+        except json.JSONDecodeError as e:
+            print(f"Error decoding JSON: {e}")
+            
+    def saveData():
+        print("")
+        
+        
+    log_file_path = "C:\\Users\\owenf.DESKTOP-LV44FQI\\OneDrive\\Documents\\Magic-The-Gathering-Draft-Grader\\UTC_Log - 12-03-2024 22.17.25.log"
